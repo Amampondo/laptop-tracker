d16097007c0b2fbcf3ebdea95284ba4efa105852
Mluleki Amampondo Mkhunyana <120003479+Amampondo@users.noreply.github.com>
2026-05-15 14:04:38 +0200

    Update App.jsx

--- tracker/dashboard/src/App.jsx
+++ tracker/dashboard/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './AuthContext'
-import { useLocationReporter } from './useLocationReporter'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import OrgList from './pages/OrgList'
@@ -15,29 +14,19 @@ function Guard({ children, roles }) {
   return children
 }
 
-// Sits inside AuthProvider so it can read the logged-in user
-function TrackerCore() {
-  const { user } = useAuth()
-  useLocationReporter(user)   // starts/stops reporting as user logs in or out
-
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/orgs" element={<Guard roles={['super']}><OrgList /></Guard>} />
-      <Route path="/orgs/:orgId" element={<Guard roles={['super', 'manager']}><OrgDetail /></Guard>} />
-      <Route path="/orgs/:orgId/register" element={<Guard roles={['super', 'manager']}><RegisterUser /></Guard>} />
-      <Route path="/users/:userId" element={<Guard><UserDetail /></Guard>} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  )
-}
-
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <TrackerCore />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/orgs" element={<Guard roles={['super']}><OrgList /></Guard>} />
+          <Route path="/orgs/:orgId" element={<Guard roles={['super', 'manager']}><OrgDetail /></Guard>} />
+          <Route path="/orgs/:orgId/register" element={<Guard roles={['super', 'manager']}><RegisterUser /></Guard>} />
+          <Route path="/users/:userId" element={<Guard><UserDetail /></Guard>} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
       </BrowserRouter>
     </AuthProvider>
   )
